803932c5b0d07c604f80d9910cf4c531d551c40a
Andrii Zinchenko <z@jerasoft.net>
2017-05-29 18:00:12 +0300

    Initial release - test one

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# precommit-pylint
+precommit-pylint
+==========
+
+Hook for pylint with configurable score limit.
+
+
+## Installation
+
+`pip install precommit-pylint`
+
+
+## Console scripts
+
+```
+precommit-pylint --help
+usage: reorder-python-imports [-h] [--diff-only] [--add-import ADD_IMPORT]
+                              [--remove-import REMOVE_IMPORT]
+                              [--application-directories APPLICATION_DIRECTORIES]
+                              [filenames [filenames ...]]
+
+positional arguments:
+  filenames
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --diff-only           Show unified diff instead of applying reordering.
+  --add-import ADD_IMPORT
+                        Import to add to each file. Can be specified multiple
+                        times.
+  --remove-import REMOVE_IMPORT
+                        Import to remove from each file. Can be specified
+                        multiple times.
+  --application-directories APPLICATION_DIRECTORIES
+                        Colon separated directories that are considered top-
+                        level application directories. Defaults to `.`
+```
+
+## As a pre-commit hook
+
+See [pre-commit](https://github.com/pre-commit/pre-commit) for instructions
+
+Hooks available:
+- `precommit-pylint` - This hook runs pylint and allows configurable score limit
+
---
+++ precommit_pylint/main.py
@@ -0,0 +1,114 @@
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import argparse
+import os
+import re
+import decimal
+import subprocess
+
+_SCORE_REGEXP = re.compile(
+    r'^Your\ code\ has\ been\ rated\ at\ (\-?[0-9\.]+)/10')
+
+_IGNORE_REGEXT = re.compile(
+    r'Ignoring entire file \(file\-ignored\)'
+)
+
+def _parse_score(pylint_output):
+    """Parse the score out of pylint's output as a float
+
+    If the score is not found, return 0.0.
+
+    """
+    for line in pylint_output.splitlines():
+        match = re.match(_SCORE_REGEXP, _futurize_str(line))
+        if match:
+            return float(match.group(1))
+    return 0.0
+
+def _check_ignore(pylint_output):
+    """Check the python file whether ignored
+    If the file is ignored returns True,
+    returns False otherwise
+    """
+    for line in pylint_output.splitlines():
+        match = re.search(_IGNORE_REGEXT, _futurize_str(line))
+        if match:
+            return True
+
+    return False
+
+def check_file(limit, filename):
+    """Check single file
+
+    :type limit: float
+    :param limit: Minimum score to pass the commit
+    :type filename: str
+    :param filename: Path to the file to check
+    """
+
+    # Check if file to skip
+    if filename == '__init__.py':
+        if os.stat(filename).st_fize == 0:
+            print(
+                'Skipping pylint on {} (empty __init__.py..'
+                '\tSKIPPED'.format(filename))
+            return True
+
+    # Start pylint
+    print('Running pylint on {}..\t'.format(filename))
+
+    try:
+        command = ['pylint', filename]
+        proc = subprocess.Popen(
+            command,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE)
+
+        out, _ = proc.communicate()
+    except OSError:
+        print("\nAn error occurred. Is pylint installed?")
+        return False
+
+    # Verify the score
+    score = _parse_score(out)
+    ignored = _check_ignore(out)
+    if ignored or score >= float(limit):
+        status = 'PASSED'
+    else:
+        status = 'FAILED'
+
+    # Add some output
+    print('{:.2}/10.00\t{}{}'.format(
+        decimal.Decimal(score),
+        status,
+        ignored and '\tIGNORED' or ''))
+
+    # If failed
+    if status == 'FAILED':
+        return False
+
+    return True
+
+def main(argv=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filenames', nargs='*')
+    parser.add_argument(
+        '--limit', type=float, default=10,
+        help=(
+            'Score limit for pylint, defaults to `%(default)s`'
+        ),
+    )
+    args = parser.parse_args(argv)
+
+    # check files
+    for filename in args.filenames:
+        if not check_file(args.limit, filename):
+            return 1
+
+    return 0
+
+
+if __name__ == '__main__':
+    exit(main())
